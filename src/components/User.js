1a82ac435b72001887b9e3881b9f2a2c69e6973d
steven opio <stevenviko@gmail.com>
2021-07-17 03:13:44 +0300

    Add user data

---
+++ src/components/User.js
@@ -0,0 +1,53 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import { Checkbox, Avatar, Typography, withStyles } from "@material-ui/core"
+import { ListItem, ListItemSecondaryAction } from "@material-ui/core"
+
+const styles = theme => ({
+  notSelected: {
+    backgroundColor: theme.palette.text.disabled
+  },
+  selected: {
+    backgroundColor: "transparent"
+  },
+  bigAvatar: {
+    width: 60,
+    height: 60,
+    marginRight: 20
+  }
+})
+
+const User = ({ classes, isSelected, user }) => (
+  <ListItem
+    dense
+    button
+    className={isSelected ? classes.notSelected : classes.selected}
+  >
+    <Avatar className={classes.bigAvatar} src={user.avatarURL} />
+    <Typography variant="title">{user.name}</Typography>
+    <ListItemSecondaryAction>
+    <Checkbox checked={isSelected} />
+    </ListItemSecondaryAction>
+  </ListItem>
+)
+
+User.propTypes = {
+  classes: PropTypes.shape({
+    bigAvatar: PropTypes.string.isRequired,
+    notSelected: PropTypes.string.isRequired,
+    selected: PropTypes.string.isRequired
+  }).isRequired,
+  isSelected: PropTypes.bool.isRequired,
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    avatarURL: PropTypes.string.isRequired
+  }).isRequired
+}
+
+const mapStateToProps = ({ users }, { id }) => ({
+  user: users[id]
+})
+
+export default connect(mapStateToProps)(withStyles(styles)(User))
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from "redux"
+import questions from "./questions"
+import authedUser from "./authedUser"
 import users from "./users"
 
-export default combineReducers({ users })
+export default combineReducers({ users,authedUser, questions  })
